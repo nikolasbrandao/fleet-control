aa3e6214370a50c4ad4b39616162dd681f2efa70
nikolasbrandao <nikolasbrandao@gmail.com>
2020-05-26 20:05:12 -0300

    fix: adjust width style

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -4,9 +4,9 @@ const PageWrapper = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 85vw;
+  width: 90vw;
   height: 90vh;
-  margin-top: ${({ marginTop }) => (marginTop ? marginTop : '5vh')};
+  margin: ${({ margin }) => (margin ? margin : '5vh')};
   background-color: rgba(246, 247, 249);
   justify-content: center;
 
