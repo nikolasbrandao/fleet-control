6a3b10aab47a2704977f1aaac5d932f87549893a
nikolasbrandao <nikolasbrandao@gmail.com>
2020-05-26 22:48:29 -0300

    feat: protected routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import Header from './components/Header'
 
 import LoginPage from './pages/Login'
 import FrotaPage from './pages/Frota'
 
+const RequireAuth = ({ children }) => {
+  const token = localStorage.getItem('@fleet-control/token')
+  if (token) {
+    return children
+  }
+  return <Redirect to='/login' />
+}
+
 function App() {
   return (
     <Switch>
+      <Route exact path='/'>
+        {localStorage.getItem('@fleet-control/token') ? (
+          <Redirect to='/veiculos' />
+        ) : (
+          <Redirect to='/login' />
+        )}
+      </Route>
       <Route path='/login'>
         <LoginPage />
       </Route>
-      <BrowserRouter>
-        <Header />
-        <Route path='/veiculos'>
-          <FrotaPage />
-        </Route>
-      </BrowserRouter>
+      <RequireAuth>
+        <BrowserRouter>
+          <Header />
+          <Route path='/veiculos'>
+            <FrotaPage />
+          </Route>
+        </BrowserRouter>
+      </RequireAuth>
     </Switch>
   )
 }
